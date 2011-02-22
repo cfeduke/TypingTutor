277b5f1ff80b744676a7b720b9f796f59dd78ab1
Charles Feduke <charles.feduke@gmail.com>
2011-02-22 08:09:57 -0500

    colors

---
+++ ColorFormatter.h
@@ -0,0 +1,16 @@
+//
+//  ColorFormatter.h
+//  TypingTutor
+//
+//  Created by Charles Feduke on 2/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ColorFormatter : NSFormatter {
+	NSColorList *colorList;
+}
+
+@end
